e0c197b24bf103aae9bbc3e4347c677e0f101308
Tomasz Rzęsikowski <tomasz.rzesikowski@gmail.com>
2021-03-27 13:14:32 +0100

    feat(cars): create Car model

--- cars/models.py
+++ cars/models.py
@@ -6,3 +6,11 @@ class Manufacturer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey("Manufacturer", on_delete=models.CASCADE, null=False)
+    model = models.CharField(max_length=150, null=False, default=None)
+
+    def __str__(self):
+        return f"{self.manufacturer.name} {self.model}"
--- cars/tests/tests_models/tests_car_model.py
+++ cars/tests/tests_models/tests_car_model.py
@@ -1,7 +1,7 @@
 from django.db import DataError, IntegrityError
 from django.test import TestCase
 
-from cars.models import Manufacturer
+from cars.models import Manufacturer, Car
 
 
 class CarTestWithoutDBConnection(TestCase):
@@ -36,18 +36,20 @@ class CarTestWithoutDBConnection(TestCase):
         self.assertEqual(car.model, "Mustang")
 
     def test_car_str(self):
-        self.assertEqual(self.car.__str__(), self.car.model)
+        manufacturer=Manufacturer(id=1, name="Ford")
+        car = Car(manufacturer=manufacturer, model="Mustang")
+        self.assertEqual(car.__str__(), "Ford Mustang")
 
 
 class CarTestWithDBConnection(TestCase):
     def setUp(self):
         self.manufacturer = Manufacturer.objects.create(name="Ford")
+        self.car = Car.objects.create(model="Mustang", manufacturer=self.manufacturer)
 
     def test_car_create_with_proper_fields(self):
-        car = Car.objects.create(name="Mustang", manufacturer_id=self.manufacturer)
-        self.assertNotEqual(car.id, None)
-        self.assertEqual(car.model, "Mustang")
-        self.assertEqual(car.manufacturer_id, self.manufacturer.id)
+        self.assertNotEqual(self.car.id, None)
+        self.assertEqual(self.car.model, "Mustang")
+        self.assertEqual(self.car.manufacturer_id, self.manufacturer.id)
 
     def test_car_empty_manufacturer_id_field(self):
         with self.assertRaises(IntegrityError):
@@ -55,8 +57,13 @@ class CarTestWithDBConnection(TestCase):
 
     def test_car_empty_model_field(self):
         with self.assertRaises(IntegrityError):
-            Car.objects.create(manufacturer_id=self.manufacturer)
+            Car.objects.create(manufacturer=self.manufacturer)
 
     def test_car_too_long_model_field(self):
         with self.assertRaises(DataError):
-            Car.objects.create(manufacturer_id=self.manufacturer, name="t" * 151)
+            Car.objects.create(manufacturer=self.manufacturer, model="t" * 151)
+
+    def test_car_manufacturer_cascade_on_del(self):
+        self.manufacturer.delete()
+        with self.assertRaises(Car.DoesNotExist):
+            Car.objects.get(pk=self.car.id)
