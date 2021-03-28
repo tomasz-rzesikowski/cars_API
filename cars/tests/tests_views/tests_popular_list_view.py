2415f4a9d3d049e0bd3e21e1949352319df9afb7
Tomasz Rzęsikowski <tomasz.rzesikowski@gmail.com>
2021-03-28 12:40:10 +0200

    test(cars): create tests for popular list view

---
+++ cars/tests/tests_views/tests_popular_list_view.py
@@ -0,0 +1,48 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase, APIRequestFactory
+
+from cars.models import Car, Manufacturer, Rate
+from cars.serializers import CarGetSerializer, PopularSerializer
+
+factory = APIRequestFactory()
+
+
+class CarListViewTest(APITestCase):
+    def setUp(self) -> None:
+        self.view = PopularListView.as_view()
+        self.request = factory.get(reverse("cars:list_popular"))
+
+    def test_url_revers(self):
+        url = reverse("cars:list_popular")
+        self.assertEqual(url, "/popular/")
+
+    def test_empty_popular_list(self):
+        cars = Car.objects.all()
+        serializer = PopularSerializer(cars, many=True)
+
+        response = self.view(self.request)
+        response.render()
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_not_empty_popular_list(self):
+        manufacturer = Manufacturer.objects.create(make="Ford")
+
+        car_one = Car.objects.create(manufacturer=manufacturer, model="Mustang")
+        car_two = Car.objects.create(manufacturer=manufacturer, model="F-150")
+
+        Rate.objects.create(car=car_one, rating=1)
+        Rate.objects.create(car=car_one, rating=5)
+
+        Rate.objects.create(car=car_two, rating=4)
+
+        cars = Car.objects.all()
+        serializer = PopularSerializer(cars, many=True)
+
+        response = self.view(self.request)
+        response.render()
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
